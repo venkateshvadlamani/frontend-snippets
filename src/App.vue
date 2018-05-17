f2a52558831f6bb58651b683471d90908c6ce3d0
venkatesh <venkatesh.caretiger@gmail.com>
2018-05-16 22:32:34 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <div class="container">
+
+      <!-- NAVBAR -->
+      <div class="navbar">Frontend Snippets</div>
+
+      <!-- SIDEBAR -->
+      <div class="sidebar">
+        <!-- Css-grid -->
+        <h3>CSS-Grid</h3>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link :to="{name: 'About'}">About</router-link></li>
+          <li><router-link to="/responsivecards">Responsive Cards</router-link></li>
+        </ul>
+
+
+
+        <!-- Axios -->
+
+        <!-- Vuex -->
+
+
+      </div>
+
+      <!-- CONTENT -->
+      <div class="content">
+        <router-view></router-view>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Snippets"
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 25px;
+}
+
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+}
+
+h3 {
+  border: 3px solid black;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 2fr 7fr;
+  grid-row: 60px auto;
+}
+
+.navbar {
+  border: 2px solid green;
+  grid-column: 1/3;
+  grid-row: 1/2;
+  background-color: #2c3e50;
+  color: white;
+}
+.sidebar {
+  grid-column: 1/2;
+  grid-row: 2/3;
+  background-color: lightgrey;
+  height: 100vh;
+}
+.content {
+  grid-column: 2/3;
+  grid-row: 2/3;
+  height: 100vh;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #259864;
+}
+</style>
---
+++ src/components/ResponsiveCards.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="responsivecards">
+
+  <main>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Paleo_food.jpg">
+      <h3>1) All of These Headlines Are Not Exactly The Same</h3>
+      <p>In the real world, you don't know how long the content will be. Writers will write what they need. Photos will be cropped to the shape that they should be. That's good. Make a system that allows this. You want it to be flexible and robust.</p>
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Ravenna_food.jpg">
+      <h3>12) Size, Isn't That Magical!</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris tindunt.</p> 
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Restaurant_food_Kunming_Yunnan.jpg">
+      <h3>7) The New CSS Will Fix Things</h3>
+      <p>Finally we get some real hammers.</p>
+    </article>
+    <article>    
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Bunter_Teller.jpg">
+      <h3>8) Several Kinds of Hammers</h3>
+      <p>Grid, Flexbox, Alignment, Shapes.</p> 
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/P3-Egyptian_food_Koshary.jpg">
+      <h3>9) Size, Isn't That Magical!</h3>
+      <p>Lorem ipsum dolor sit amet.</p> 
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Mexican_food.jpg">
+      <h3>10) All of These Headlines</h3>
+      <p>Always three lines of text, which fits exactly in the available space. Describing where you will go when you click.</p>
+    </article>
+    <article>    
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/P2-Arepitas_Food_Macro.jpg">
+      <h3>11) Are Exactly the Same</h3>
+      <p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. </p>     
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Ravenna_food.jpg">
+      <h3>12) Size, Isn't That Magical!</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris tindunt.</p> 
+    </article>
+
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Paleo_food.jpg">
+      <h3>1) All of These Headlines Are Not Exactly The Same</h3>
+      <p>In the real world, you don't know how long the content will be. Writers will write what they need. Photos will be cropped to the shape that they should be. That's good. Make a system that allows this. You want it to be flexible and robust.</p>
+    </article>
+    <article>    
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Indian_bengali_food.jpg">
+      <h3>2) Some Are Short</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.</p> 
+    </article>
+    <article>
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/Food_in_Miyajima.jpg">
+      <h3>3) Some Headlines Can Sometimes Be Quite Long, Because It Takes Time to Make A Point</h3>
+      <p>Vivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis.</p> 
+    </article>        
+  </main>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResponsiveCards",
+  data() {
+    return {
+      msg: "ResponsiveCards"
+    };
+  }
+};
+</script>
+
+<style>
+.responsivecards {
+
+}
+</style>
